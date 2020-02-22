b425572027f3c179a206ba47b4e12e6178385a4b
Tom Grigg <tomegrigg@gmail.com>
2020-02-22 09:44:54 -0800

    Hangman kata -- refactor w/streaming State object
    Replace the imperative while loop with a streaming object that
    manages state transitions.

--- hangman/src/main/scala/Hangman.scala
+++ hangman/src/main/scala/Hangman.scala
@@ -4,19 +4,50 @@
  */
 import java.io.EOFException
 
+import scala.collection.{AbstractIterable, AbstractIterator}
 import scala.io.{Source, StdIn}
 import scala.util.Random
 
-/*
- *  Gallows
- *     ___        ___        ___       ___        ___        ___          ___
- *    |  |       |  |       |  |      |  |       |  |       |  |        |  |
- *       |       o  |       o  |      o  |       o  |       o  |        o  |
- *       |          |       |  |     \|  |      \|/ |      \|/ |       \|/ |
- *       |          |          |         |          |      /   |       / \ |
- *    ___|___    ___|___    ___|___   ___|___    ___|___    ___|___     ___|___
- *
- */
+class Guesses extends Iterable[Char] {
+  @scala.annotation.tailrec
+  private def nextGuess(): Char = {
+    try {
+      print("Enter your next guess: ")
+      StdIn.readChar()
+    } catch {
+      case _: EOFException => nextGuess()
+      case _: StringIndexOutOfBoundsException => nextGuess()
+    }
+  }
+
+  override def iterator: Iterator[Char] = new AbstractIterator[Char] {
+    override def hasNext: Boolean = true
+    override def next(): Char = nextGuess()
+  }
+}
+
+class StateGen[I, N, S](initial: I => S)(transition: (S, N) => S)(seed: I) {    // TODO can I replace this with anything built in?  Iterator.unfold?
+  def run(inputs: Iterable[N]): Iterable[S] = new AbstractIterable[S] {
+    override def iterator: Iterator[S] = new AbstractIterator[S] {
+      private[this] var state: S = null.asInstanceOf[S]  // FIXME!
+      private[this] val underlying = inputs.iterator
+
+      override def hasNext: Boolean = {
+        if (state == null) true
+        else underlying.hasNext
+      }
+      override def next(): S = {
+        if (state == null) {
+          state = initial(seed)
+        } else {
+          val n = underlying.next()
+          state = transition(state, n)
+        }
+        state
+      }
+    }
+  }
+}
 
 
 object Hangman {
@@ -25,11 +56,16 @@ object Hangman {
   case object Won extends TerminationState
   case object Lost extends TerminationState
 
-  case class GameState(badGuesses: Int, wordState: List[Option[Char]], guessedSoFar: Set[Char])
+  type WordState = List[Option[Char]]
 
-  val MAX_GUESSES = 6
+  case class GameState(
+                        word: String,
+                        badGuesses: Int,
+                        termState: TerminationState,
+                        wordState: WordState,
+                        guessedSoFar: Set[Char])
 
-  val gallows: Array[String] = Array(
+  private[this] val gallows: Array[String] = Array(
     "  ---\n |  |\n    |\n    |\n    |\n    |\n ___|___",
     "  ---\n |  |\n o  |\n    |\n    |\n    |\n ___|___",
     "  ---\n |  |\n o  |\n |  |\n    |\n    |\n ___|___",
@@ -39,6 +75,8 @@ object Hangman {
     "  ---\n |  |\n o  |\n\\|/ |\n/ \\ |\n    |\n ___|___",
   )
 
+  private[this] val MAX_GUESSES = gallows.length - 1
+
   def selectRandomWord(): String = {
     val infile = Source.fromFile("word-list.txt")
     val words = infile.getLines().toArray
@@ -47,88 +85,67 @@ object Hangman {
     words(index)
   }
 
-  def initialGameState(word: String): GameState = GameState(0, List.fill(word.length)(None), Set.empty)
+  def initialGameState(word: String): GameState =
+    GameState(word, 0, InProgress, List.fill(word.length)(None), Set.empty)
 
-  def nextGameState(word: String, state: GameState, guess: Char): GameState = {
-    if (word.contains(guess)) {
+  def nextGameState(state: GameState, guess: Char): GameState = {
+    if (state.word.contains(guess)) {
       // zip word with state.wordState, update where letter matches guess
-      val zipped = word.toList zip state.wordState
-      val newWordState = zipped map {
+      val zipped = state.word.toList zip state.wordState
+      val wordState = zipped map {
         case (c, _) if c == guess => Some(c)
         case (_, ws) => ws
       }
-      GameState(state.badGuesses, newWordState, state.guessedSoFar + guess)
+      def hasWon(s: WordState) = !s.contains(None)
+      val termState = if (hasWon(wordState)) Won else state.termState
+      state.copy(wordState=wordState, termState=termState, guessedSoFar=state.guessedSoFar + guess)
     } else {
-      GameState(state.badGuesses + 1, state.wordState, state.guessedSoFar + guess)
+      val badGuesses = state.badGuesses + 1
+      val termState = if (badGuesses == MAX_GUESSES) Lost else state.termState
+      state.copy(badGuesses=badGuesses, termState=termState, guessedSoFar=state.guessedSoFar + guess)
     }
   }
 
-  def hasWon(state: GameState): Boolean = !state.wordState.contains(None)
+  def wordDisplay(word: List[Option[Char]]): String = word.map {
+    case Some(c) => s"$c "
+    case None => "_ "
+  }.reduce(_ + _)
 
-  def terminationState(state: GameState): TerminationState = {
-    if (hasWon(state)) Won
-    else if (state.badGuesses == MAX_GUESSES) Lost
-    else InProgress
-  }
-
-  // State:s
-  //  number of incorrect guesses
-  //  correct guesses by position
-
-  def wordStateDisplay(word: List[Option[Char]]): String = word.map {
-      case Some(c) => s"$c "
-      case None => "_ "
-    }.reduce(_ + _)
-
-  def gallowsState(badGuesses: Int): String = gallows(badGuesses)
+  def gallowsDisplay(badGuesses: Int): String = gallows(badGuesses)
 
   def showGameState(state: GameState): Unit = {
-    println(gallowsState(state.badGuesses))
+    println(gallowsDisplay(state.badGuesses))
     println()
-    println(wordStateDisplay(state.wordState))
+    println(wordDisplay(state.wordState))
     println(s"Guessed so far: ${new String(state.guessedSoFar.toArray.sorted)}")
-  }
 
-  @scala.annotation.tailrec
-  def nextGuess(): Char = {
-    try {
-      StdIn.readChar()
-    } catch {
-      case _: EOFException => nextGuess()
-      case _: StringIndexOutOfBoundsException => nextGuess()
+    state.termState match {
+      case Won =>
+        println("You win!")
+      case Lost =>
+        println("You lose!")
+        println(s"The word was ${state.word}")
+      case _ => ()
     }
   }
 
   def main(args: Array[String]): Unit = {
-    val word = selectRandomWord()
-    var state = initialGameState(word)
+    val word = selectRandomWord()    // TODO why is type inference failing for 'Char' ?
+    val sg: StateGen[String, Char, GameState] = new StateGen(initialGameState)(nextGameState)(word)
 
     //println(word)   // debugging
-    showGameState(state)
+    val stream = sg.run(new Guesses).iterator
 
-    while (true) {
-      print("Enter your next guess: ")
-      val guess = nextGuess()
-
-      state = nextGameState(word, state, guess)
+    stream.foreach { state =>
       showGameState(state)
-
-      terminationState(state) match {
-        case Won =>
-          println("You win!")
-          return
-        case Lost =>
-          println("You lose!")
-          println(s"The word was $word")
-          return
-        case _ => ()
-      }
+      if (state.termState != InProgress)
+        return
     }
+
+
+    // TODO this does not take the last element (Won/Lost)
+    //stream.takeWhile(_.termState == InProgress).foreach(showGameState)
+
   }
-
-  type Initial[I, S] = I => S
-  type Transition[N, S] = (S, N) => S
-
-  // create an Iterable[N] to generate the game input (per-letter user input)
 
 }
